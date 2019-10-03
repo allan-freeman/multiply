d11fe75ea1351a104cc0fda2ed35ff24a1b68379
Alexander Ivanushkin <allanfreeman757@gmail.com>
2019-10-03 20:54:43 +0300

    feat: add task multiply

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  // your solution  
+let first_num = first.split('').reverse();
+let second_num = second.split('').reverse();
+let mult_result = [];
+
+for (let i = 0; i < first_num.length; i++) {
+  for (let j = 0; j < second_num.length; j++) {
+    let m = first_num[i] * second_num[j];
+    mult_result[i + j] = (mult_result[i + j]) ? mult_result[i + j] + m : m;
+  }  
 }
+
+for (let i = 0; i < mult_result.length; i++) {
+  let num = mult_result[i] % 10;
+  let move = Math.floor(mult_result[i] / 10);
+  mult_result[i] = num;
+
+  if (mult_result[i + 1])
+    mult_result[i + 1] += move;
+  else if (move != 0)
+    mult_result[i + 1] = move;
+}
+
+return mult_result.reverse().join('');     
+}
+
